9fa6f88b0f50941805157ab3e626289da6f32d94
penrose15 <seongju99@naver.com>
2025-10-11 22:10:05 +0900

    Test: 도메인 객체에 id 추가기능 메서드 생성

--- src/test/java/tobyspring/splearn/domain/member/MemberFixture.java
+++ src/test/java/tobyspring/splearn/domain/member/MemberFixture.java
@@ -1,5 +1,7 @@
 package tobyspring.splearn.domain.member;
 
+import org.springframework.test.util.ReflectionTestUtils;
+
 public class MemberFixture {
 
     public static Member createMemberRegister(MemberRegisterRequest memberRegisterRequest, PasswordEncoder passwordEncoder) {
@@ -26,5 +28,19 @@ public class MemberFixture {
 
     public static MemberRegisterRequest createMemberRegisterRequest() {
         return createMemberRegisterRequest("toby@splearn.com");
+    }
+
+    public static Member createMember() {
+        return Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+    }
+
+    public static Member createMember(Long id) {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        ReflectionTestUtils.setField(member, "id", id);
+        return member;
+    }
+
+    public static Member createMember(String email) {
+        return Member.register(createMemberRegisterRequest(email), createPasswordEncoder());
     }
 }
